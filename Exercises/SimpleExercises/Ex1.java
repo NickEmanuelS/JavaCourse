27e7bc5539390ff67658515e8b6a0f0e3efa5b48
NickEmanuelS <nicolasdipinho@gmail.com>
2022-06-29 08:29:16 -0300

    Commit

---
+++ Exercises/SimpleExercises/Ex1.java
@@ -0,0 +1,80 @@
+package SimpleExercises;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		
+		Rectangle rectangle;
+		
+		System.out.print("Enter width size: ");
+		double width = kb.nextDouble();
+		
+		System.out.print("Enter height size: ");
+		double height = kb.nextDouble();
+		
+		rectangle = new Rectangle(width, height);
+		
+		System.out.println(rectangle);
+		
+		kb.close();
+	}
+
+}
+
+class Rectangle{
+	private double width, height;
+	
+	public Rectangle(){
+		
+	}
+	public Rectangle(double width, double height){
+		this.setWidth(width);
+		this.setHeight(height);
+	}
+	/**
+	 * @return the width
+	 */
+	public double getWidth() {
+		return width;
+	}
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	/**
+	 * @return the height
+	 */
+	public double getHeight() {
+		return height;
+	}
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public double area() {
+		return (this.width * this.height);
+	}
+	
+	public double perimeter() {
+		return (this.width * 2) + (this.height * 2);
+	}
+	
+	public double diagonal() {
+		double p = ((Math.pow(this.width, 2)) + (Math.pow(this.height, 2)));
+		return Math.sqrt(p);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Area = " + this.area() + "\nPerimeter = " + this.perimeter() + "\nDiagonal = " +this.diagonal());
+	}
+}
